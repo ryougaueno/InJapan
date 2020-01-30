dad55b979fba68869e48f4d7c5245babab2143d0
ryougaueno <ryougaueno@gmail.com>
2020-01-30 12:09:50 +0800

    わかりませｎ

--- html/app7.js
+++ html/app7.js
@@ -1,12 +1,27 @@
 
 $(function() {
-    var $allMsg = $('#title');
-    var $wordList = $('#title').html().split("");
-    $('#title').html("");
+	var $allMsg = $('.title');
+	
+    var $wordList = $('.title').html().split("");
+    $('.title').html("");
     $.each($wordList, function(idx, elem) {
-        var newEL = $("<span/>").text(elem).css({ opacity: 0 });
+
+		var newEL = $("<span/>").text(elem).css({ opacity: 0 });
+		
         newEL.appendTo($allMsg);
         newEL.delay(idx * 70);
         newEL.animate({ opacity: 1 }, 1100);
     });
 });
+
+// $(function() {
+//     var $allMsg = $('.title2');
+//     var $wordList = $('.title2').html().split("");
+//     $('.title2').html("");
+//     $.each($wordList, function(idx, elem) {
+//         var newEL = $("<span/>").text(elem).css({ opacity: 0 });
+//         newEL.appendTo($allMsg);
+//         newEL.delay(idx * 70);
+//         newEL.animate({ opacity: 1 }, 1100);
+//     });
+// });
