112cc3009d2dcb8c7cd89c1133149668b04a4d07
ryougaueno <ryougaueno@gmail.com>
2020-01-30 09:58:02 +0800

    start fadein background-fixed

--- app.js
+++ app.js
@@ -21,4 +21,15 @@
         pageTop();
     });
 }());
-// hambagar
+
+
+// start
+
+$(function() {
+	setTimeout(function(){
+		$('.start p').fadeIn(1600);
+	},500); //0.5秒後にロゴをフェードイン!
+	setTimeout(function(){
+		$('.start').fadeOut(500);
+	},2500); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
+});
